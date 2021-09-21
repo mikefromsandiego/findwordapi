1500fd2ba76856de2aed6d4496a1cd539e7f60f0
mikefromsandiego <d.r.scholefield@talk21.com>
2021-09-21 12:33:07 +0100

    Initial project creation

---
+++ src/main/java/org/goldendogconsulting/findwordapi/FindwordRestApiApplication.java
@@ -0,0 +1,13 @@
+package org.goldendogconsulting.findwordapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FindwordRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FindwordRestApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/goldendogconsulting/findwordapi/FindwordRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.goldendogconsulting.findwordapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FindwordRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
